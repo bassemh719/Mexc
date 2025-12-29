491ba793fce52cfffcdc589162cb3725194227b9
bassemh719 <bassemahashem@gmail.com>
2025-12-29 07:53:30 +0200

    Implement fetch function to handle API requests

---
+++ Index.js
@@ -0,0 +1,31 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    const endpoint = url.searchParams.get("endpoint");
+
+    if (!endpoint) {
+      return new Response(
+        JSON.stringify({ error: "Missing endpoint" }),
+        {
+          status: 400,
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      );
+    }
+
+    const mexcUrl = "https://api.mexc.com" + endpoint;
+
+    const res = await fetch(mexcUrl);
+    const data = await res.text();
+
+    return new Response(data, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+  }
+};
